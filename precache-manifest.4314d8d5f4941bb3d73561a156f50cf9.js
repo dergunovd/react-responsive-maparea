2256759c047ff21c5694d3168b9244dfef46f85d
Dmitry Dergunov <dmitry@dergunov.net>
2021-11-06 05:17:37 +0300

    Updates

---
+++ precache-manifest.4314d8d5f4941bb3d73561a156f50cf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e917d82f203043b16903df88db2d2c8e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e9829e8d1eb0d45093d",
+    "url": "./static/js/2.483d291b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.483d291b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9da94dfb04186197dd8b",
+    "url": "./static/js/main.a0b70ea9.chunk.js"
+  },
+  {
+    "revision": "dabe476fecd6a609709b",
+    "url": "./static/js/runtime-main.00edcc6b.js"
+  },
+  {
+    "revision": "8fc7e91fa30c1e5c8ebe18d6a92a1b8c",
+    "url": "./static/media/img.8fc7e91f.png"
+  }
+]);
